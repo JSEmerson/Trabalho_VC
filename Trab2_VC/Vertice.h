6d91b1c1f303384dc805610f889cde24291c1473
JSEmerson <emerson.silva@engenharia.ufjf.br>
2017-05-30 14:49:56 -0300

    Segundo commit
    - Classe vertice adicionada;

---
+++ Trab2_VC/Vertice.h
@@ -0,0 +1,20 @@
+#ifndef TRAB2_VC_PONTO_H
+#define TRAB2_VC_PONTO_H
+
+class Vertice
+{
+private:
+    float x,y,z,intensidade;
+public:
+    Vertice();
+    void definirVertice(float xx, float yy, float it);
+    void definirVertice(float xx, float yy, float zz, float it);
+    float consultarX();
+    float consultarY();
+    float consultarZ();
+    float consultarIntensidade();
+    ~Vertice();
+};
+
+#endif //TRAB2_VC_PONTO_H
+
---
+++ Trab2_VC/cmake-build-debug/CMakeFiles/Trab2_VC.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Trab2_VC.dir/main.cpp.o"
+  "CMakeFiles/Trab2_VC.dir/Vertice.cpp.o"
+  "Trab2_VC.pdb"
+  "Trab2_VC"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Trab2_VC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
