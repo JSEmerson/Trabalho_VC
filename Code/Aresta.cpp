a41995134271a3b67c824d114a48e8e9bee797ec
Guilherme Felix <guinga4z@gmail.com>
2017-05-30 14:51:47 -0300

    Estrutura Aresta implementada - Falta testar

---
+++ Code/Aresta.cpp
@@ -0,0 +1,35 @@
+#include Aresta.h
+
+//Construtor da classe
+Aresta::Aresta()
+{
+	vertice1 = NULL;
+	vertice2 = NULL;
+}
+
+Vertice* Aresta::consultarVertice1()
+{
+	return vertice1;
+}
+
+Vertice* Aresta::consultarVertice2()
+{
+	return vertice2;
+}
+
+void Aresta::definirVertice1(Vertice* v1)
+{
+	vertice1 = v1;
+}
+
+void Aresta::definirVertice2(Vertice* v2)
+{
+	vertice2 = v2;
+}
+
+//Destrutor da classe
+Aresta::~Aresta()
+{
+	delete vertice1;
+	delete vertice2;
+}
---
+++ Code/Aresta.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include Vertice.h
+
+class Aresta
+{
+	private:
+		Vertice* vertice1;
+		Vertice* vertice2;
+
+	public:
+		Aresta();
+		Vertice* consultarVertice1();
+		Vertice* consultarVertice2();
+		void definirVertice1(Vertice* v1);
+		void definirVertice2(Vertice* v2);
+		~Aresta();
+
+
+}
